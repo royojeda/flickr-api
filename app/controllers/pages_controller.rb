2c7b78a3dfdbcac7e14852b3d2f9bc147dae0a9f
Dennis Ojeda <royojeda1@gmail.com>
2022-07-12 21:37:45 +0800

    Fix bug with text_field default value

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   def index
     @flickr = Flickr.new Figaro.env.flickr_api_key, Figaro.env.flickr_api_secret
     @photos = @flickr.people.getPhotos user_id: params[:user_id] if params[:user_id]
+    @user_id = params[:user_id] || "196042883@N07"
   end
 end
